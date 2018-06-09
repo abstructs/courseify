18498f863ce4d2688aeea4488da23f1d84272795
abstructs <andrewclarke540@gmail.com>
2018-06-09 15:47:30 -0400

    unroll first fully working profile

--- courseify_client/src/Profile.js
+++ courseify_client/src/Profile.js
@@ -24,38 +24,38 @@ class ProfileEdit extends Component {
                             
                         <div className="col-md-4">
                             <label for="last_name">Last Name</label>
-                            <input onChange={this.props.handleUserInfoChange} type="text" className="form-control" name="last_name" placeholder="Last Name" required />
+                            <input onChange={this.props.handleUserInfoChange} value={this.props.new_user_info.last_name} type="text" className="form-control" name="last_name" placeholder="Last Name" required />
                         </div>
 
                         <div className="col-md-4">
                             <label for="headline">Headline</label>
-                            <input onChange={this.props.handleUserInfoChange} type="text" className="form-control" name="headline" placeholder="Job Title" required />
+                            <input onChange={this.props.handleUserInfoChange} value={this.props.new_user_info.headline} type="text" className="form-control" name="headline" placeholder="Job Title" required />
                         </div> 
                     </div>
 
                     <div className="form-row mb-4">
                         <div className="col-md-6">
                             <label for="country">Country</label>
-                            <input onChange={this.props.handleUserInfoChange} type="text" className="form-control" name="country" placeholder="Country" required />
+                            <input onChange={this.props.handleUserInfoChange} value={this.props.new_user_info.country} type="text" className="form-control" name="country" placeholder="Country" required />
                         </div>
                                 
                         <div className="col-md-6">
-                            <label for="school">Education</label>
-                            <input onChange={this.props.handleUserInfoChange} type="text" className="form-control" name="school" placeholder="School" required />
+                            <label for="education">Education</label>
+                            <input onChange={this.props.handleUserInfoChange} value={this.props.new_user_info.education} type="text" className="form-control" name="education" placeholder="Education" required />
                         </div>
                     </div>
 
                     <div className="form-row mb-4">
                         <div className="col-md-12">
                             <label for="industry">Industry</label>
-                            <input onChange={this.props.handleUserInfoChange} type="text" className="form-control" name="industry" placeholder="Industry" required />
+                            <input onChange={this.props.handleUserInfoChange} value={this.props.new_user_info.industry} type="text" className="form-control" name="industry" placeholder="Industry" required />
                         </div>
                     </div>
 
                     <div className="form-row mb-4">
                         <div className="col-md-12">
                             <label for="summary">Summary</label>
-                            <textarea onChange={this.props.handleUserInfoChange} className="form-control" name="summary" placeholder="Summary" required />
+                            <textarea onChange={this.props.handleUserInfoChange} value={this.props.new_user_info.summary} className="form-control" name="summary" placeholder="Summary" required />
                         </div>
                     </div>
                 </form>
@@ -71,11 +71,19 @@ class ProfileInfo extends Component {
 
     render() {
         return (
-            <div className="col-xl-4 mt-4 mr-auto text-center">
+            <div className="col-xl-4 m-4 text-center text-justify">
                 {/* <h2 className="text-light font-weight-light p-auto">Andrew Wilson</h2> */}
-                <p className="d-inline pr-3"><b>0</b> recommendations</p>
-                <p className="d-inline pr-3"><b>0</b> followers</p>
-                <p className="d-inline pr-3"><b>0</b> following</p>
+                {/* <div className="text-center "> */}
+                    <p className="d-inline pr-3"><b>0</b> recommendations</p>
+                    <p className="d-inline pr-3"><b>0</b> followers</p>
+                    <p className="d-inline pr-3"><b>0</b> following</p>
+                {/* </div> */}
+                <br/>
+                <br/>
+                <p className=""><b><i>{this.props.user_info.headline}</i></b> in <b><i>{this.props.user_info.country}</i></b>.</p>
+                <p>Involved in <b><i>{this.props.user_info.industry}</i></b>.</p>
+                <p className="">Attended <b><i>{this.props.user_info.education}</i></b>.</p>
+                <p className="" style={{whiteSpace: "pre-wrap"}}><i>{this.props.user_info.summary}</i></p>
 
             </div>
         );
@@ -90,15 +98,7 @@ class Profile extends Component {
             save: false,
 
             // for profile edit
-            new_user_info: {
-                first_name: "test",
-                last_name: "", 
-                headline: "",
-                education: "",
-                industry: "",
-                country: "",
-                summary: ""
-            },
+            new_user_info: {},
 
             // user info
             user_info: {}
@@ -116,6 +116,8 @@ class Profile extends Component {
                 user_info: res.data.user,
                 new_user_info: res.data.user
             })
+
+            console.log(res.data.user)
         })
         .catch(err => {
             console.log(err);
@@ -162,12 +164,12 @@ class Profile extends Component {
             return <Redirect to='/'/>;
         }
 
-        const middleSection = this.state.edit ? <ProfileEdit new_user_info={this.state.new_user_info} handleUserInfoChange={this.handleUserInfoChange.bind(this)} /> : <ProfileInfo />
-
         if(Object.keys(this.state.user_info).length == 0) {
             return <div>Loading</div>
         }
 
+        const middleSection = this.state.edit ? <ProfileEdit new_user_info={this.state.new_user_info} handleUserInfoChange={this.handleUserInfoChange.bind(this)} /> : <ProfileInfo user_info={this.state.user_info}/>
+        
         return (
             <div>
                 <header className="bg-dark border-0 pt-5" style={{marginBottom: "0px", height: "150px", backgroundImage: "url(" + teacherImage +")", backgroundPosition: "250px 660px"}}>
@@ -181,11 +183,11 @@ class Profile extends Component {
                             <div className="mb-2 mt-4 text-center">
                             {
                                 !this.state.edit ?
-                                <a href="#edit" className="btn text-white m-auto text-center" style={{width: "250px", backgroundColor: "#ff6000"}} onClick={this.handleEdit.bind(this)}>Edit</a>
+                                <a href="#edit" className="btn m-2 text-white m-auto text-center" style={{width: "250px", backgroundColor: "#ff6000"}} onClick={this.handleEdit.bind(this)}>Edit</a>
                                 :
                                 <div>
                                     <a href="#save" className="btn text-white m-auto text-center" style={{width: "250px", backgroundColor: "#ff6000"}} onClick={this.handleSave.bind(this)}>Save</a>
-                                    <a href="#cancel" className="btn text-white m-auto text-center btn-primary" style={{width: "250px"}} onClick={this.handleCancel.bind(this)}>Cancel</a>
+                                    <a href="#cancel" className="btn text-white m-2 text-center btn-primary" style={{width: "250px"}} onClick={this.handleCancel.bind(this)}>Cancel</a>
                                 </div>
                             }
                                 {/* <a href="#edit" className="btn text-white m-auto text-center" style={{width: "250px", backgroundColor: "#ff6000"}} onClick={this.handleEdit.bind(this)}>{!this.state.edit ? "Edit" : "Save"}</a> */}
