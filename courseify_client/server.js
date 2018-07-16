e0b6b7578833f73b91a45a64d8468bdf86566e4d
abstructs <andrewclarke540@gmail.com>
2018-07-16 14:33:44 -0400

    configure rails for production in docker

--- courseify_client/server.js
+++ courseify_client/server.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use('/build', express.static('static'));
 app.use(express.static('build'));
-app.get('/', function (req, res) {
-  res.sendFile(`${__dirname}/public/`);
-})
+app.use('build', express.static('static'));
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/build/service-worker.js`);
+  res.sendFile(`${__dirname}/build/`);
+});
+
+
 
 app.listen(PORT, _ => {
     console.log(`Express server is now listening on PORT=${PORT}`);
